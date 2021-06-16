079708521d9f8a5fdccfbba2cdd372c945c05164
skmehta123 <85477626+skmehta123@users.noreply.github.com>
2021-06-16 08:58:58 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -155,7 +155,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirror Bot',
+            'description': 'Uploaded by Torrbot',
             'mimeType': mime_type,
         }
         try:
@@ -523,9 +523,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirror Bot Search',
-                                 author_name='Slam Mirror Bot',
-                                 author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                 title = 'Torrbot Search',
+                                 author_name='Torrbot',
+                                 author_url='https://telegram.dog/gtgyan',
                                  html_content=content)
         return
 
@@ -608,9 +608,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirror Bot Search',
-                                                        author_name='Slam Mirror Bot',
-                                                        author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                                        title = 'Torrbot Search',
+                                                        author_name='Torrbot',
+                                                        author_url='https://telegram.dog/gtgyan',
                                                         html_content=content
                                                         )['path'])
 
